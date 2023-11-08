a1a05c15b7548a68b3a3a341293cd68ddeca7827
Sergey Tyurin <sergey.tyurin@ligastavok.ru>
2023-11-08 12:43:37 +0300

    added file

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello World')
+# print('Hello World')
+
+
+import random
+import string
+
+
+def generate_random_key(length):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
+
+print(generate_random_key(44))
